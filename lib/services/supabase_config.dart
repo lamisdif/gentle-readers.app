4bf74be9bc1ffa7d7833b628b03e865961c728ee
Lamis Dif <lamisdif@gmail.com>
2026-04-16 10:38:41 +0200

    Fix Supabase URL typov

--- lib/services/supabase_config.dart
+++ lib/services/supabase_config.dart
@@ -3,7 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class SupabaseConfig {
   static Future<void> initialize() async {
     await Supabase.initialize(
-      url: 'https://fsximdlrhlglabxbqvay.supabase.co',
+      url: 'https://fsximdllrhglabxbqvay.supabase.co',
       anonKey:
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzeGltZGxscmhnbGFieGJxdmF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3NTg0NzgsImV4cCI6MjA3MjMzNDQ3OH0.KiRJdFoW4DtDAPMLqH9Im3-37GhIFmD269iDsY7ih2Q',
     );
